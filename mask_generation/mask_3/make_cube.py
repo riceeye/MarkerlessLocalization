c9578e89f5a64be12125354902f56b677c8b360e
Tim Lezun Li <timli@Tim-Li-MacBookAir-3.local>
2025-04-01 16:04:40 -0400

    red cube images

--- mask_generation/mask_3/make_cube.py
+++ mask_generation/mask_3/make_cube.py
@@ -2,12 +2,12 @@ from PIL import Image
 
 # Define the image size and grey color
 width, height = 512, 512
-grey_color = (128, 128, 128)  # RGB values for grey
+color = (220, 0, 0)  # RGB values for grey
 
 # Create a new image with the specified color
-image = Image.new("RGB", (width, height), grey_color)
+image = Image.new("RGB", (width, height), color)
 
 # Save the image to a PNG file
-image.save("arm_texture.png")
+image.save("texture.png")
 
 print("Texture file 'cube_texture.png' created successfully.")
